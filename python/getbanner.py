6bb0aee75adf2cbc1558e57324aaf7fb1f0aa243
Stu Baker <stubakers@gmail.com>
2022-03-17 03:36:54 -0600

    adding Cyan Scyther and Fateful banners, images
    - also updating Default Encounter for new character, rates

--- python/getbanner.py
+++ python/getbanner.py
@@ -116,9 +116,9 @@ def scrape_html(url):
 
 
 def main():
-    url = 'https://api-us.another-eden.games/asset/lottery_notice/view/87b94c2b3d6754a9017f9cba584e880e?language=en'
-    banner_name = 'Fateful Encounter 3rd Character Popularity Vote'
-    rate_up = ['Thillelille','Thillelille(AS)','Suzette','Suzette(AS)','Pizzica','Isuka','Isuka(AS)','Isuka(ES)','Daisy','Melissa','Necoco','Tsubame','Tsubame(AS)','Myunfa','Myunfa(AS)','Tsukiha','Tsukiha(AS)','Tsukiha(ES)']
+    url = 'https://api-us.another-eden.games/asset/lottery_notice/view/45851dd4857dd1b54afe72fb878402e7?language=en'
+    banner_name = 'Encounter Cyan Scyther Wind Force Edition'
+    rate_up = ['Cyan Scyther','Cynthia','Felmina','Claude(AS)']
 
     scrape_html(url)
 
