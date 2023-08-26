6c130bff927b60dcf2decb5ba77f0b39e64f519d
Stu Baker <stubakers@gmail.com>
2023-08-26 05:21:36 -0600

    adding Elseal and Fateful banners, images
    - also updating Default Encounter for new character, rates

--- python/getbanner.py
+++ python/getbanner.py
@@ -117,7 +117,7 @@ def scrape_html(url, is_sidekick_col_on_banner):
 
 
 def main():
-    url = 'https://api-us.another-eden.games/asset/lottery_notice/view/5d0ba6902b7ec580f83c9ba750ce56ec?language=en'
+    url = 'https://api-us.another-eden.games/asset/lottery_notice/view/188899e9258cebcd53cca484b8bc08bb?language=en'
     banner_name = 'Default Encounter'
     rate_up = []
     is_sidekick_col_on_banner = True
