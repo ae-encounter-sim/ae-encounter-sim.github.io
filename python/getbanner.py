b9deb9d8d048e9436cc3ac25566f4b84bf90af3c
Stu Baker <stubakers@gmail.com>
2023-09-17 17:20:18 -0600

    adding Melody AS and Fateful banners, images
    - also updating Default Encounter for new character, rates

--- python/getbanner.py
+++ python/getbanner.py
@@ -117,7 +117,7 @@ def scrape_html(url, is_sidekick_col_on_banner):
 
 
 def main():
-    url = 'https://api-us.another-eden.games/asset/lottery_notice/view/34c4cd1f3e39bb15ca4211aa230f7363?language=en'
+    url = 'https://api-us.another-eden.games/asset/lottery_notice/view/034f7280174f05b6f742310b6af1111b?language=en'
     banner_name = 'Default Encounter'
     rate_up = []
     is_sidekick_col_on_banner = True
